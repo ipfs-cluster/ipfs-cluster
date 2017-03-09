b19b3c6882a44d7bbcf09a2da35634db8077fe9b
Hector Sanjuan <hector@protocol.ai>
2017-03-09 12:41:52 +0100

    Release 0.0.7
    License: MIT
    Signed-off-by: Hector Sanjuan <hector@protocol.ai>

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package ipfscluster
 
 // Version is the current cluster version. Version alignment between
 // components, apis and tools ensures compatibility among them.
-const Version = "0.0.4"
+const Version = "0.0.7"
 
 // Commit is the current build commit of cluster. See Makefile
 var Commit string
