39683460c696ec9b7fcb2eab1451a9ad9c90e22e
Hector Sanjuan <hector@protocol.ai>
2017-03-28 11:57:11 +0200

    Release 0.0.11
    License: MIT
    Signed-off-by: Hector Sanjuan <hector@protocol.ai>

--- ipfs-cluster-ctl/main.go
+++ ipfs-cluster-ctl/main.go
@@ -23,7 +23,7 @@ const programName = `ipfs-cluster-ctl`
 
 // Version is the cluster-ctl tool version. It should match
 // the IPFS cluster's version
-const Version = "0.0.10"
+const Version = "0.0.11"
 
 var (
 	defaultHost     = fmt.Sprintf("127.0.0.1:%d", 9094)
