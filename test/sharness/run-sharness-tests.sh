14a047c2f332a2d9d053a443a6258891ac750900
Wyatt <wdaviau@cs.stanford.edu>
2017-04-20 12:38:40 -0700

    Build fully automated, including aggregation

--- test/sharness/run-sharness-tests.sh
+++ test/sharness/run-sharness-tests.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 #
 # MIT License
+
+# Run tests 
 cd "$(dirname "$0")"
 for i in `ls t*.sh | sort`;
 do
     echo "*** $i ***"
     ./$i 
 done
+
+# Aggregate Results
+echo "We are aggregating"
+for f in test-results/*.counts; do
+    echo "$f";
+done | bash aggregate-results.sh
