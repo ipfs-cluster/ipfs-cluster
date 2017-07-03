99217ea84e3dd67911ba52c3ad907e9e4b51d741
ZenGround0 <wtd37@cornell.edu>
2017-07-03 09:49:25 -0700

    Travis sharness (#110)
    * Added travis sharness build
    
    * ipfs-cluster-ctl help text width overflow fixed
    
    * Expect success from fixed sharness test
    
    * First travis-sharness PR touch up
    Undoes gx rewrite of cluster-ctl dependencies
    Undoess linebreaking of usage descriptions
    Changes test to check for lines in excess of 120 chars instead of 80
    run-sharness script now correctly tracks exit codes and exits with error

--- sharness/run-sharness-tests.sh
+++ sharness/run-sharness-tests.sh
@@ -2,10 +2,12 @@
 
 # Run tests
 cd "$(dirname "$0")"
+exit_codes=()
 for i in t*.sh;
 do
     echo "*** $i ***"
     ./$i
+    exit_codes+=($?)
 done
 
 # Aggregate Results
@@ -13,3 +15,10 @@ echo "Aggregating..."
 for f in test-results/*.counts; do
     echo "$f";
 done | bash lib/sharness/aggregate-results.sh
+
+# If any test fails error on exit
+for exit_code in "${exit_codes[@]}"; do
+    if [[ $exit_code != 0 ]]; then
+        exit 1
+    fi
+done
--- sharness/t0010-ctl-basic-commands.sh
+++ sharness/t0010-ctl-basic-commands.sh
@@ -21,11 +21,11 @@ test_expect_success "cluster-ctl help commands succeed" '
     ipfs-cluster-ctl help
 '
 
-test_expect_failure "cluster-ctl help has 80 char limits" '
+test_expect_success "cluster-ctl help has 120 char limits" '
     ipfs-cluster-ctl --help >help.txt &&
     test_when_finished "rm help.txt" &&
     LENGTH="$(cat help.txt | awk '"'"'{print length }'"'"' | sort -nr | head -n 1)" &&
-    [ ! "$LENGTH" -gt 80 ]
+    [ ! "$LENGTH" -gt 120 ]
 '
 
 test_expect_success "cluster-ctl help output looks good" '
@@ -47,15 +47,15 @@ test_expect_success "cluster-ctl commands output looks good" '
     egrep -q "ipfs-cluster-ctl commands" commands.txt
 '
 
-test_expect_success "All cluster-ctl command docs are 80 columns or less" '
+test_expect_success "All cluster-ctl command docs are 120 columns or less" '
     export failure="0" &&
     ipfs-cluster-ctl commands | awk "NF" >commands.txt &&
     test_when_finished "rm commands.txt" &&
     while read cmd
     do
         LENGTH="$($cmd --help | awk "{ print length }" | sort -nr | head -n 1)"
-        [ "$LENGTH" -gt 80 ] &&
-            { echo "$cmd" help text is longer than 79 chars "($LENGTH)"; export failure="1"; }
+        [ "$LENGTH" -gt 120 ] &&
+            { echo "$cmd" help text is longer than 119 chars "($LENGTH)"; export failure="1"; }
     done <commands.txt
 
     if [ $failure -eq "1" ]; then
