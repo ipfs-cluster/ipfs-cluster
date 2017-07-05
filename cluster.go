ec0451d9e842fc947d14e671e6c3a0d0a887bde4
Hector Sanjuan <hector@protocol.ai>
2017-07-05 18:53:05 +0200

    Print current candidates in the error when there are not enough
    License: MIT
    Signed-off-by: Hector Sanjuan <hector@protocol.ai>

--- cluster.go
+++ cluster.go
@@ -1112,8 +1112,8 @@ func (c *Cluster) allocate(hash *cid.Cid, repl int, blacklist []peer.ID) ([]peer
 		return validAllocations[0 : len(validAllocations)+needed], nil
 	case candidatesValid < needed:
 		err = logError(
-			"not enough candidates to allocate %s. Needed: %d. Got: %d",
-			hash, needed, candidatesValid)
+			"not enough candidates to allocate %s. Needed: %d. Got: %s",
+			hash, needed, candidates)
 		return nil, err
 	default:
 		// this will return candidate peers in order of
@@ -1128,7 +1128,7 @@ func (c *Cluster) allocate(hash *cid.Cid, repl int, blacklist []peer.ID) ([]peer
 		// we don't have enough peers to pin
 		if len(candidateAllocs) < needed {
 			err = logError(
-				"cannot find enough allocations for %s. Needed: %d. Got: %d",
+				"cannot find enough allocations for %s. Needed: %d. Got: %s",
 				hash, needed, candidateAllocs)
 			return nil, err
 		}
