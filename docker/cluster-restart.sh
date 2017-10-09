d6e1c39e86338b7d8d656262b208a11c0b124067
Wyatt <wdaviau@stanford.edu>
2017-10-09 11:43:34 -0400

    Include cluster-restart script for test image

---
+++ docker/cluster-restart.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Restart the cluster process
+sleep 2
+while true; do
+  pgrep ipfs-cluster-service || ipfs-cluster-service -f init; echo "CLUSTER RESTARTED"; ipfs-cluster-service --debug &
+  sleep 10
+done
