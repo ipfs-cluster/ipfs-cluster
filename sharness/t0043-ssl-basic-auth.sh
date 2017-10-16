ce9fc18b197c6f92cce17d224989a568ef9e5ec8
dgrisham <dgrisham@mines.edu>
2017-10-16 11:04:29 -0600

    Adding + fixing sharness tests for BasicAuth

--- sharness/t0043-ssl-basic-auth.sh
+++ sharness/t0043-ssl-basic-auth.sh
@@ -15,8 +15,14 @@ test_expect_success "prerequisites" '
     test_have_prereq IPFS && test_have_prereq CLUSTER
 '
 
+test_expect_success "ssl interaction fails with bad credentials" '
+    id=`cluster_id`
+    { test_must_fail ipfs-cluster-ctl --no-check-certificate --basic-auth "testuser:badpass" id; } | grep -A1 "401" | grep "Unauthorized"
+'
+
 test_expect_success "ssl interaction succeeds" '
     id=`cluster_id`
+    ipfs-cluster-ctl --no-check-certificate --basic-auth "userwithoutpass" id | egrep -q "$id" &&
     ipfs-cluster-ctl --no-check-certificate --basic-auth "testuser:testpass" id | egrep -q "$id"
 '
 
