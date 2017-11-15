3726ef8c9d7ab4c5d7ddddc6e70ff617fbfd0d7a
Hector Sanjuan <code@hector.link>
2017-11-15 23:39:22 +0100

    Include a tag annotation, sign tags
    License: MIT
    Signed-off-by: Hector Sanjuan <code@hector.link>

--- release.sh
+++ release.sh
@@ -12,5 +12,9 @@ fi
 sed -i "s/const Version.*$/const Version = \"$version\"/" version.go
 sed -i "s/const Version.*$/const Version = \"$version\"/" ipfs-cluster-ctl/main.go
 git commit -a -m "Release $version"
-git tag v$version
+lastver=`git tag -l | grep -E 'v[0-9]+\.[0-9]+\.[0-9]+' | tail -n 1`
+echo "Tag for Release ${version}" > tag_annotation
+echo >> tag_annotation
+git log --pretty=oneline ${lastver}..HEAD >> tag_annotation
+git tag -a -s -F tag_annotation v$version
 gx release $version 
