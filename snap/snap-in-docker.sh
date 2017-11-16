22c1d2401f5a688fe6b65e2e1df912e646601227
Leo Arias <leo.arias@canonical.com>
2017-11-16 14:36:28 +0000

    Move back the docker call to travis

--- snap/snap-in-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Build the ipfs-cluster snaps and push them to the store using a docker container.
-
-set -ev
-
-docker run -v $(pwd):$(pwd) -t snapcore/snapcraft sh -c "apt update -qq && cd $(pwd) && ./snap/snap-multiarch.sh"
