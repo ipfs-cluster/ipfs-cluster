16c9188fd4854857e510939b5bfd33e2768daa8a
Wyatt Daviau <wdaviau@cs.stanford.edu>
2018-02-05 16:42:52 -0500

    printing importer works
    License: MIT
    Signed-off-by: Wyatt Daviau <wdaviau@cs.stanford.edu>

--- api/rest/restapi.go
+++ api/rest/restapi.go
@@ -13,7 +13,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	//	"io"
 	"mime"
 	"net"
 	"net/http"
@@ -36,6 +35,7 @@ import (
 	ma "github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr-net"
 	dex "github.com/zenground0/dex"
+	//	"io"
 )
 
 var logger = logging.Logger("restapi")
@@ -515,8 +515,7 @@ func (api *API) addFileHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err := dex.ImportToPrint(f)
-	/*
-		buf := make([]byte, 256)
+	/*	buf := make([]byte, 256)
 		for {
 			file, err := f.NextFile()
 			if err == io.EOF {
@@ -536,7 +535,8 @@ func (api *API) addFileHandler(w http.ResponseWriter, r *http.Request) {
 				fmt.Printf(string(buf[:n]))
 			}
 			fmt.Printf(string(buf[:n]))
-		}*/
+		}
+	*/
 
 	sendAcceptedResponse(w, err)
 }
