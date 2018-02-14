ceba32f8719a08f4f98ac1a4d3bb02d9a44e67c0
Wyatt Daviau <wdaviau@cs.stanford.edu>
2018-02-13 23:44:43 -0500

    Test for ipfscon:blockPut
    License: MIT
    Signed-off-by: Wyatt Daviau <wdaviau@cs.stanford.edu>

--- test/cids.go
+++ test/cids.go
@@ -10,6 +10,8 @@ var (
 	TestCid1     = "QmP63DkAFEnDYNjDYBpyNDfttu1fvUw99x1brscPzpqmmq"
 	TestCid2     = "QmP63DkAFEnDYNjDYBpyNDfttu1fvUw99x1brscPzpqmma"
 	TestCid3     = "QmP63DkAFEnDYNjDYBpyNDfttu1fvUw99x1brscPzpqmmb"
+	TestCid4     = "Qma24zbcWc6vAcQ7CP1v1FyePuTRxYNnaAyNtDfXDgwRsP"
+	TestCid4Data = "Cid4Data" // Cid resulting from block put NOT ipfs add
 	TestSlowCid1 = "QmP63DkAFEnDYNjDYBpyNDfttu1fvUw99x1brscPzpqmmd"
 	// ErrorCid is meant to be used as a Cid which causes errors. i.e. the
 	// ipfs mock fails when pinning this CID.
