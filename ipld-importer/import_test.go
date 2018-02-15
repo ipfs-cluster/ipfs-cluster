03f3eb59b95e4163a44d4ae1dd27ae3a81e01e8d
Wyatt Daviau <wdaviau@cs.stanford.edu>
2018-02-14 19:02:55 -0500

    Addressing second round of comments
    License: MIT
    Signed-off-by: Wyatt Daviau <wdaviau@cs.stanford.edu>

--- ipld-importer/import_test.go
+++ ipld-importer/import_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"github.com/ipfs/go-ipfs-cmdkit/files"
-	ipld "github.com/ipfs/go-ipld-format"
 )
 
 const testDir = "testingData"
@@ -39,7 +38,7 @@ func TestToPrint(t *testing.T) {
 	}
 }
 
-var orderedCids = [18]string{"QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn",
+var cids = [18]string{"QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn",
 	"Qmbp4C4KkyjVzTpZ327Ub555FEHizhJS4M17f2zCCrQMAz",
 	"QmYz38urZ99eVCxSccM63bGtDv54UWtBDWJdTxGch23khA",
 	"QmUwG2mfhhfBEzVtvyEvva1kc8kU4CrXphdMCdAiFNWdxy",
@@ -49,10 +48,10 @@ var orderedCids = [18]string{"QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn",
 	"QmcUBApwNSDg2Q2J4NXzks1HewVGFohNpPyEud7bZfo5tE",
 	"QmaKaB735eydQwnaJNuYbXRU1gVb4MJdzHp1rpUZJN67G6",
 	"QmQ6n82dRtEJVHMDLW5BSrJ6bRDXkFXj2i7Vjxy4B2PH82",
-	"QmbDaWrwxk93RfWSBL8ajTproNKbE2EghBLSk8199EBm1m",
-	"QmXDmRPxV9KWYLhN7bqGn143GgiNxhy9Hqc3ZPJW63xgok",
 	"QmPZLJ3CZYgxH4K1w5jdbAdxJynXn5TCB4kHy7u8uHC3fy",
 	"QmUqmcdJJodX7mPoUv9u71HoNAiegBCiCqdUZd5ZrCyLbs",
+	"QmbDaWrwxk93RfWSBL8ajTproNKbE2EghBLSk8199EBm1m",
+	"QmXDmRPxV9KWYLhN7bqGn143GgiNxhy9Hqc3ZPJW63xgok",
 	"QmaytnNarHzDp1ipGyC7zd7Hw2AePmtSpLLaygQ2e9Yvqe",
 	"QmZwab9h6ADw3tv8pzXVF2yndgJpTWKrrMjJQqRkgYmCRH",
 	"QmaNfMZDZjfqjHrFCc6tZwmkqbXs1fnY9AXZ81WUztFeXm",
@@ -65,19 +64,35 @@ func TestToChannel(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	outChan := make(chan *ipld.Node)
-	go func() {
-		i := 0
-		for node := range outChan {
-			cid := (*node).String()
-			if cid != orderedCids[i] {
-				t.Error("unexpected cid: %s instead of %s", cid, orderedCids[i])
-			}
-			i += 1
-		}
-	}()
-	err = ToChannel(file, outChan, context.Background())
+	outChan, err := ToChannel(file, context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	check := make(map[string]struct{})
+	for node := range outChan {
+		cid := (*node).String()
+		if _, ok := check[cid]; ok {
+			t.Fatalf("Duplicate cid %s", cid)
+		}
+		check[cid] = struct{}{}
+	}
+	if len(check) != len(cids) {
+		t.Fatalf("Witnessed cids: %v\nExpected cids: %v", check, cids)
+	}
+	cidsS := cids[:]
+	for cid := range check {
+		if !contains(cidsS, cid) {
+			t.Fatalf("Unexpected cid: %s", cid)
+		}
+	}
+}
+
+func contains(slice []string, s string) bool {
+	for _, a := range slice {
+		if a == s {
+			return true
+		}
+	}
+	return false
 }
