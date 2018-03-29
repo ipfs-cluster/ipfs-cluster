bebc486bb39f2cbae3133716e036a611599f3aa1
Hector Sanjuan <code@hector.link>
2018-03-29 13:47:33 +0200

    Fix release changelog when having release candidates.
    License: MIT
    Signed-off-by: Hector Sanjuan <code@hector.link>

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ make gx-clean
 sed -i "s/const Version.*$/const Version = \"$version\"/" version.go
 sed -i "s/const Version.*$/const Version = \"$version\"/" ipfs-cluster-ctl/main.go
 git commit -S -a -m "Release $version"
-lastver=`git tag -l | grep -E 'v[0-9]+\.[0-9]+\.[0-9]+' | tail -n 1`
+lastver=`git tag -l | grep -E 'v[0-9]+\.[0-9]+\.[0-9]+$' | tail -n 1`
 echo "Tag for Release ${version}" > tag_annotation
 echo >> tag_annotation
 git log --pretty=oneline ${lastver}..HEAD >> tag_annotation
