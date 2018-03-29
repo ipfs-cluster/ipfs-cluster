ed55daf2c6401ce656fdc470b3a9e06f4d145c63
Hector Sanjuan <code@hector.link>
2018-03-29 13:43:36 +0200

    Release 0.3.5
    License: MIT
    Signed-off-by: Hector Sanjuan <code@hector.link>

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package ipfscluster
 
 // Version is the current cluster version. Version alignment between
 // components, apis and tools ensures compatibility among them.
-const Version = "0.3.5-rc1"
+const Version = "0.3.5"
 
 // Commit is the current build commit of cluster. See Makefile.
 var Commit = "00000000" // actual commit set during builds.
