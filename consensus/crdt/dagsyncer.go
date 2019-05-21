44d93d61e080347e8953d7ac74b3efc4f51a729d
Hector Sanjuan <code@hector.link>
2019-05-21 11:55:48 +0200

    fix timeouts in crdt

--- consensus/crdt/dagsyncer.go
+++ consensus/crdt/dagsyncer.go
@@ -2,7 +2,6 @@ package crdt
 
 import (
 	"context"
-	"time"
 
 	ipfslite "github.com/hsanjuan/ipfs-lite"
 	cid "github.com/ipfs/go-cid"
@@ -17,17 +16,14 @@ type liteDAGSyncer struct {
 
 	ipfs       *ipfslite.Peer
 	blockstore blockstore.Blockstore
-
-	timeout time.Duration
 }
 
 func newLiteDAGSyncer(ctx context.Context, ipfs *ipfslite.Peer) *liteDAGSyncer {
 	return &liteDAGSyncer{
-		getter:     ipfs, // should we use ipfs.Session() ??
+		getter:     ipfs,
 		dags:       ipfs,
 		ipfs:       ipfs,
 		blockstore: ipfs.BlockStore(),
-		timeout:    time.Minute,
 	}
 }
 
@@ -36,38 +32,26 @@ func (lds *liteDAGSyncer) HasBlock(c cid.Cid) (bool, error) {
 }
 
 func (lds *liteDAGSyncer) Get(ctx context.Context, c cid.Cid) (ipld.Node, error) {
-	ctx, cancel := context.WithTimeout(ctx, lds.timeout)
-	defer cancel()
 	return lds.getter.Get(ctx, c)
 }
 
 func (lds *liteDAGSyncer) GetMany(ctx context.Context, cs []cid.Cid) <-chan *ipld.NodeOption {
-	ctx, cancel := context.WithTimeout(ctx, lds.timeout)
-	defer cancel()
 	return lds.getter.GetMany(ctx, cs)
 }
 
 func (lds *liteDAGSyncer) Add(ctx context.Context, n ipld.Node) error {
-	ctx, cancel := context.WithTimeout(ctx, lds.timeout)
-	defer cancel()
 	return lds.dags.Add(ctx, n)
 }
 
 func (lds *liteDAGSyncer) AddMany(ctx context.Context, nds []ipld.Node) error {
-	ctx, cancel := context.WithTimeout(ctx, lds.timeout)
-	defer cancel()
 	return lds.dags.AddMany(ctx, nds)
 
 }
 
 func (lds *liteDAGSyncer) Remove(ctx context.Context, c cid.Cid) error {
-	ctx, cancel := context.WithTimeout(ctx, lds.timeout)
-	defer cancel()
 	return lds.dags.Remove(ctx, c)
 }
 
 func (lds *liteDAGSyncer) RemoveMany(ctx context.Context, cs []cid.Cid) error {
-	ctx, cancel := context.WithTimeout(ctx, lds.timeout)
-	defer cancel()
 	return lds.dags.RemoveMany(ctx, cs)
 }
