83c486610094a99902af00bd1047640e8043ab4b
Hector Sanjuan <code@hector.link>
2019-06-13 17:27:31 +0200

    Remove Leftover println

--- consensus/raft/logging.go
+++ consensus/raft/logging.go
@@ -65,7 +65,6 @@ func (log *hcLogToLogger) Info(msg string, args ...interface{}) {
 }
 
 func (log *hcLogToLogger) Warn(msg string, args ...interface{}) {
-	fmt.Println(msg)
 	raftLogger.Warning(log.format(msg, args))
 }
 
