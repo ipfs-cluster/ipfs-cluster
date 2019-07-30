153e4f97c91f54408f5124e68c1732143ce52c37
Hector Sanjuan <code@hector.link>
2019-07-30 13:48:48 +0200

    Sharness: run state import/export with both crdt and raft

--- sharness/t0052-service-state-export.sh
+++ sharness/t0052-service-state-export.sh
@@ -7,12 +7,25 @@ test_description="Test service state export"
 test_ipfs_init
 test_cluster_init
 
-test_expect_success IPFS,CLUSTER,JQ "state export saves the correct state to expected file" '
-    cid=`docker exec ipfs sh -c "echo test_52 | ipfs add -q"` &&
+test_expect_success IPFS,CLUSTER,JQ "state export saves the correct state to expected file (crdt)" '
+    cid=`docker exec ipfs sh -c "echo test_52-1 | ipfs add -q"` &&
     ipfs-cluster-ctl pin add "$cid" &&
     sleep 5 &&
     cluster_kill && sleep 5 &&
     ipfs-cluster-service --debug --config "test-config" state export --consensus crdt -f export.json &&
+    [ -f export.json ] &&
+    jq -r ".cid | .[\"/\"]" export.json | grep -q "$cid"
+'
+
+cluster_kill
+cluster_start raft
+
+test_expect_success IPFS,CLUSTER,JQ "state export saves the correct state to expected file (raft)" '
+    cid=`docker exec ipfs sh -c "echo test_52-2 | ipfs add -q"` &&
+    ipfs-cluster-ctl pin add "$cid" &&
+    sleep 5 &&
+    cluster_kill && sleep 5 &&
+    ipfs-cluster-service --debug --config "test-config" state export --consensus raft -f export.json &&
     [ -f export.json ] &&
     jq -r ".cid | .[\"/\"]" export.json | grep -q "$cid"
 '
