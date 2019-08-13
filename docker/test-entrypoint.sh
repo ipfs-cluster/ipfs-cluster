4556cbe72691705adf4cfbe398e92ff2cc36faae
Hector Sanjuan <code@hector.link>
2019-08-13 14:43:07 +0200

    Docker: improve for CRDTs
    This:
    
      * Allows choosing the consensus for the automatic "init" on Docker with
      IPFS_CLUSTER_CONSENSUS.
    
      * Removes IPFS_PATH and the sed replacements for 0.0.0.0 on RestAPI and
      Ipfsproxy endpoints because these are easily supported by setting env vars
      correctly and pose some security risk when running with net=host.
    
      * Brings up to date Dockerfile-test and Dockerfile-bundle, even if mostly
      unused.

--- docker/test-entrypoint.sh
+++ docker/test-entrypoint.sh
@@ -33,9 +33,7 @@ if [ -e "$IPFS_CLUSTER_PATH/service.json" ]; then
     echo "Found IPFS cluster configuration at $IPFS_CLUSTER_PATH"
 else
     export CLUSTER_SECRET=""
-    ipfs-cluster-service init
-    sed -i 's/127\.0\.0\.1\/tcp\/9094/0.0.0.0\/tcp\/9094/' "$IPFS_CLUSTER_PATH/service.json"
-    sed -i 's/127\.0\.0\.1\/tcp\/9095/0.0.0.0\/tcp\/9095/' "$IPFS_CLUSTER_PATH/service.json"
+    ipfs-cluster-service init --consensus "${IPFS_CLUSTER_CONSENSUS}"
 fi
 
 ipfs-cluster-service --debug $@ &
