14e136f40ce4d92e6efd34907e87b32a26dde052
Kishan Mohanbhai Sagathiya <kishan@protocol.ai>
2019-09-23 13:13:13 +0530

    Use random available ports for listening

--- cmd/ipfs-cluster-service/main.go
+++ cmd/ipfs-cluster-service/main.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -272,6 +273,10 @@ the peer IDs in the given multiaddresses.
 					Name:  "force, f",
 					Usage: "overwrite configuration without prompting",
 				},
+				cli.BoolFlag{
+					Name:  "randomports",
+					Usage: "listen on random available ports, instead of the default values",
+				},
 			},
 			Action: func(c *cli.Context) error {
 				consensus := c.String("consensus")
@@ -319,6 +324,13 @@ the peer IDs in the given multiaddresses.
 				// Generate defaults for all registered components
 				err := cfgHelper.Manager().Default()
 				checkErr("generating default configuration", err)
+				if c.Bool("randomports") {
+					cfgs := cfgHelper.Configs()
+					assigned := make(map[int]bool)
+					cfgs.Cluster.ListenAddr = getRandomPortMultiaddr(assigned)
+					cfgs.Restapi.HTTPListenAddr = getRandomPortMultiaddr(assigned)
+					cfgs.Ipfsproxy.ListenAddr = getRandomPortMultiaddr(assigned)
+				}
 				err = cfgHelper.Manager().ApplyEnvVars()
 				checkErr("applying environment variables to configuration", err)
 
@@ -631,4 +643,20 @@ func getStateManager() cmdutils.StateManager {
 	)
 	checkErr("creating state manager", err)
 	return mgr
+}
+
+func getRandomPortMultiaddr(assigned map[int]bool) ma.Multiaddr {
+	ln, err := net.Listen("tcp", "[::]:0")
+	checkErr("creating a listener", err)
+	defer ln.Close()
+	port := ln.Addr().(*net.TCPAddr).Port
+
+	// try again if this port has already been assigned
+	if assigned[port] {
+		return getRandomPortMultiaddr(assigned)
+	}
+
+	addr, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
+	checkErr("parsing multiaddress", err)
+	return addr
 }
