9522649b0b915c5f929a657978dd38062b362332
Hector Sanjuan <code@hector.link>
2020-02-20 18:23:14 -0500

    cluster_config: format default listen addresses

--- cluster_config.go
+++ cluster_config.go
@@ -22,8 +22,13 @@ import (
 
 const configKey = "cluster"
 
-// DefaultListenAddrs contains TCP and QUIC listen addresses
-var DefaultListenAddrs = []string{"/ip4/0.0.0.0/tcp/9096", "/ip4/0.0.0.0/udp/9096/quic", "/ip6/::/tcp/9096", "/ip6/::/udp/9096/quic"}
+// DefaultListenAddrs contains TCP and QUIC listen addresses.
+var DefaultListenAddrs = []string{
+	"/ip4/0.0.0.0/tcp/9096",
+	"/ip4/0.0.0.0/udp/9096/quic",
+	"/ip6/::/tcp/9096",
+	"/ip6/::/udp/9096/quic",
+}
 
 // Configuration defaults
 const (
