7cb09c2aeaa47da48ca45e9d868c7462126a0ecd
Jorropo <jorropo.pgm@gmail.com>
2020-08-28 23:02:01 +0200

    Deprecate secio to noise.
    Closes #1214

--- clusterhost.go
+++ clusterhost.go
@@ -17,10 +17,10 @@ import (
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	dual "github.com/libp2p/go-libp2p-kad-dht/dual"
+	noise "github.com/libp2p/go-libp2p-noise"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	record "github.com/libp2p/go-libp2p-record"
-	secio "github.com/libp2p/go-libp2p-secio"
 	libp2ptls "github.com/libp2p/go-libp2p-tls"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	identify "github.com/libp2p/go-libp2p/p2p/protocol/identify"
@@ -118,7 +118,7 @@ func baseOpts(psk corepnet.PSK) []libp2p.Option {
 		libp2p.PrivateNetwork(psk),
 		libp2p.EnableNATService(),
 		libp2p.Security(libp2ptls.ID, libp2ptls.New),
-		libp2p.Security(secio.ID, secio.New),
+		libp2p.Security(noise.ID, noise.New),
 		// TODO: quic does not support private networks
 		// libp2p.Transport(libp2pquic.NewTransport),
 		libp2p.DefaultTransports,
--- go.mod
+++ go.mod
@@ -48,6 +48,7 @@ require (
 	github.com/libp2p/go-libp2p-gostream v0.2.1
 	github.com/libp2p/go-libp2p-http v0.1.5
 	github.com/libp2p/go-libp2p-kad-dht v0.8.3
+	github.com/libp2p/go-libp2p-noise v0.1.1
 	github.com/libp2p/go-libp2p-peerstore v0.2.6
 	github.com/libp2p/go-libp2p-pubsub v0.3.4
 	github.com/libp2p/go-libp2p-quic-transport v0.8.0
