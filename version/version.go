ea4dd7ed983d56c81f780fc84e150deda9e68500
Hector Sanjuan <code@hector.link>
2021-07-09 00:25:18 +0200

    Set development version v0.14.0-next

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is the current cluster version.
-var Version = semver.MustParse("0.14.0")
+var Version = semver.MustParse("0.14.0-next")
 
 // RPCProtocol is protocol handler used to send libp2p-rpc messages between
 // cluster peers.  All peers in the cluster need to speak the same protocol
