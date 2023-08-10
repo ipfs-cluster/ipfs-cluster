be612bc91d82fb98fb800631fa04d3177a66552f
Hector Sanjuan <code@hector.link>
2023-08-10 21:53:29 +0200

    Docker: use more recent versions
    Use gosu and tini from debian packages for the main container.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,7 +11,7 @@ if [ `id -u` -eq 0 ]; then
     echo "Changing user to $user"
     # ensure directories are writable
     su-exec "$user" test -w "${IPFS_CLUSTER_PATH}" || chown -R -- "$user" "${IPFS_CLUSTER_PATH}"
-    exec su-exec "$user" "$0" $@
+    exec gosu "$user" "$0" $@
 fi
 
 # Only ipfs user can get here
